e91708363ff1abf0920141e94d66a7841cefc9f7
ahmeds <ahmed-abotaleb@outlook.com>
2025-01-11 08:42:53 +0200

    notes working

--- src/main/java/com/ahmeds/superdrive/models/Note.java
+++ src/main/java/com/ahmeds/superdrive/models/Note.java
@@ -1,6 +1,5 @@
 package com.ahmeds.superdrive.models;
 
-
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,9 +8,9 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class Note {
-    private int noteid;
+    private Integer noteid;
     private String notetitle;
     private String notedescription;
-    private int userid;
+    private Integer userid;
     private User user; // reference to the User object
 }
